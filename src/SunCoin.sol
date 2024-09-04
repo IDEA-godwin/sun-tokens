5e4834952e8a22b3dc9fb4e866991c9445546c85
Godwin Igwurube <godwinaquinas@gmail.com>
2024-09-05 00:03:59 +0100

    implement deposit, mint, burn

--- src/SunCoin.sol
+++ src/SunCoin.sol
@@ -3,21 +3,21 @@ pragma solidity ^0.8.24;
 
 import "./ERC20ABC.sol";
 import "./interfaces/ISunCoin.sol";
-import {UD60x18, ud60x18} from "prb/math@4.0.3/src/UD60x18.sol";
-import {IERC721} from "@openzeppelin/contracts@5.0.2/interfaces/IERC721.sol";
 import {ISunCoin} from "./interfaces/ISunCoin.sol";
 
 /**
  * @title Sun
  * @notice Token contract implementing a linear sDAI-backed bonding curve where the solpe is 25 basis points (0.0025).
- * @dev Adheres to ERC-20 token standard and uses the ERC-4626 tokenized vault interface for bonding curve operations.
+ * @dev Adheres to ERC-20 token standard and uses the ERC-4626 tokenized vault interface.
  */
 contract SunCoin is ERC20ABC, ISunCoin {
+    uint16 buffer = 1;
+    uint256 totalAssetsHold;
 
     ERC20 public constant USDT = ERC20(0x05D032ac25d322df992303dCa074EE7392C117b9);
 
     /**
-     * @dev Constructs the SunCoin contract, checks the sDAI token address and the fee account address.
+     * @dev Constructs the SunCoin contract, checks the USDT token address.
      */
     constructor() ERC20("SunCoin", "SNC") ERC20Permit("SunCoin") {
         if (address(USDT) == address(0)) revert CannotBeZero();
@@ -32,15 +32,6 @@ contract SunCoin is ERC20ABC, ISunCoin {
     }
 
     /**
-     * @dev See {IERC4626-withdraw}.
-     */
-    function withdraw(uint256 assets, address receiver, address owner) external returns (uint256 shares) {
-        uint256 fee;
-        (shares, fee) = computeWithdraw(assets, totalSupply());
-        _withdraw(receiver, owner, assets, shares, fee);
-    }
-
-    /**
      * @dev See {IERC4626-mint}.
      */
     function mint(uint256 shares, address receiver) external returns (uint256 assets) {
@@ -48,19 +39,15 @@ contract SunCoin is ERC20ABC, ISunCoin {
         _deposit(receiver, assets, shares);
     }
 
-    /**
-     * @dev See {IERC4626-redeem}.
-     */
-    function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets) {
-        uint256 fee;
-        (shares, assets, fee) = computeRedeem(shares, totalSupply());
-        _withdraw(receiver, owner, assets, shares, fee);
+    function currentPrice() external returns (uint256 price) {
+        uint256 supply = totalSupply() + buffer;
+        price = supply / totalAssetsHold;
     }
 
-    /**
-     * @dev Implements {IERC4626-deposit} and protects againts slippage by specifying a minimum number of shares to receive.
-     * @param minSharesOut The minimum number of shares the sender expects to receive.
-     */
+    function totalAssets() external view returns (uint256 totalManagedAssets) {
+        totalManagedAssets = totalAssetsHold;
+    }
+
     function safeDeposit(uint256 assets, address receiver, uint256 minSharesOut) external returns (uint256 shares) {
         shares = computeDeposit(assets, totalSupply());
         if (shares < minSharesOut) revert SlippageError();
@@ -75,10 +62,7 @@ contract SunCoin is ERC20ABC, ISunCoin {
     external
     returns (uint256 shares)
     {
-        uint256 fee;
-        (shares, fee) = computeWithdraw(assets, totalSupply());
-        if (shares > maxSharesIn) revert SlippageError();
-        _withdraw(receiver, owner, assets, shares, fee);
+        revert();
     }
 
     /**
@@ -99,85 +83,35 @@ contract SunCoin is ERC20ABC, ISunCoin {
     external
     returns (uint256 assets)
     {
-        uint256 fee;
-        (shares, assets, fee) = computeRedeem(shares, totalSupply());
-        if (assets < minAssetsOut) revert SlippageError();
-        _withdraw(receiver, owner, assets, shares, fee);
+        revert();
     }
 
     /**
-     * @dev See {IERC4626-previewDeposit}.
+    * @dev See {IERC4626-previewDeposit}.
      */
     function previewDeposit(uint256 assets) external view returns (uint256 shares) {
-        return computeDeposit(assets, totalSupply());
+        revert();
     }
 
     /**
      * @dev See {IERC4626-previewWithdraw}.
      */
     function previewWithdraw(uint256 assets) external view returns (uint256 shares) {
-        uint256 fee;
-        if (totalAssets() < assets) revert Undersupply();
-        (shares, fee) = computeWithdraw(assets, totalSupply());
-        return shares + fee;
+        revert();
     }
 
     /**
      * @dev See {IERC4626-previewMint}.
      */
     function previewMint(uint256 shares) external view returns (uint256 assets) {
-        return computeMint(shares, totalSupply());
+        revert();
     }
 
     /**
      * @dev See {IERC4626-previewRedeem}.
      */
     function previewRedeem(uint256 shares) external view returns (uint256 assets) {
-        if (totalSupply() < shares) revert Undersupply();
-        (, assets,) = computeRedeem(shares, totalSupply());
-    }
-
-    /**
-     * @notice Computes the current price of a share, as price = totalSupply * slope + 0
-     * which is derived from the linear slope function: f(x) = mx + c,
-     * where `x` is the supply of shares, the slope `m` is 0.0025, and `c` is a constant term = 0.
-     *
-     * @return price The current price along the bonding curve.
-     */
-    function currentPrice() external view returns (UD60x18) {
-        return ud60x18(totalSupply()).mul(SLOPE);
-    }
-
-    /**
-     * @dev See {IERC4626-convertToShares}.
-     */
-    function convertToShares(uint256 assets) external view returns (uint256 shares) {
-        if (totalAssets() < assets) revert Undersupply();
-        UD60x18 conversionPrice = ud60x18(totalSupply()).mul(HALF_SLOPE);
-        shares = ud60x18(assets).div(conversionPrice).intoUint256();
-    }
-
-    /**
-     * @dev See {IERC4626-convertToAssets}.
-     */
-    function convertToAssets(uint256 shares) external view returns (uint256 assets) {
-        if (totalSupply() < shares) revert Undersupply();
-        UD60x18 conversionPrice = ud60x18(totalSupply()).mul(HALF_SLOPE);
-        assets = ud60x18(shares).mul(conversionPrice).intoUint256();
-    }
-
-    /**
-     * @dev See {IERC4626-maxWithdraw}.
-     */
-    function maxWithdraw(address owner) external view returns (uint256 maxAssets) {
-        (, maxAssets,) = computeRedeem(balanceOf(owner), totalSupply());
-    }
-
-    /**
-     * @dev See {IERC4626-maxRedeem}.
-     */
-    function maxRedeem(address owner) external view returns (uint256 maxShares) {
-        return balanceOf(owner);
+        revert();
     }
 
     /**
@@ -195,84 +129,69 @@ contract SunCoin is ERC20ABC, ISunCoin {
     }
 
     /**
+     * @dev See {IERC4626-maxWithdraw}.
+     */
+    function maxWithdraw(address owner) external view returns (uint256 maxAssets) {
+        revert();
+    }
+
+    /**
+     * @dev See {IERC4626-maxRedeem}.
+     */
+    function maxRedeem(address owner) external view returns (uint256 maxShares) {
+        return balanceOf(owner);
+    }
+
+    /**
      * @dev See {IERC4626-asset}.
      */
     function asset() external pure returns (address assetTokenAddress) {
-        return address(SDAI);
+        return address(USDT);
     }
 
     /**
-     * @dev See {IERC4626-totalAssets}.
+    * @dev See {IERC4626-convertToAssets}.
      */
-    function totalAssets() public view returns (uint256 totalManagedAssets) {
-        totalManagedAssets = SDAI.balanceOf(address(this));
+    function convertToAssets(uint256 shares) external view returns (uint256 assets) {
+        if (totalSupply() < shares) revert Undersupply();
+        uint256 ratio = totalSupply() / totalAssetsHold;
+        assets = shares * ratio;
     }
 
     /**
-     * @notice Computes the number of shares to be minted for a given amount of assets to be deposited.
-     * Utilizes the equation y = sqrt((0.00125x^2 + A) / 0.00125) - x, derived from the trapezium area formula.
-     *
-     * @param assets The amount of assets to be deposited.
-     * @param totalSupply The total supply of shares in the system.
-     * @return shares The calculated number of shares minted for the deposited assets.
+      * @dev See {IERC4626-convertToShares}.
      */
-    function computeDeposit(uint256 assets, uint256 totalSupply) public pure returns (uint256 shares) {
-        UD60x18 initialTotal = ud60x18(totalSupply);
-        UD60x18 total = initialTotal.add(ud60x18(assets))
-        shares = initialSupply.add(ud60x18(assets).div(HALF_SLOPE)).sqrt().sub(initialSupply).intoUint256();
+    function convertToShares(uint256 assets) external view returns (uint256 shares) {
+        if (totalAssetsHold < assets) revert Undersupply();
+
+        uint256 ratio = totalAssetsHold / totalSupply();
+        shares = assets * ratio;
     }
 
-    /**
-     * @notice Computes the number of shares to be burned and the exit fee for a given amount of assets to be withdrawn.
-     * Utilizes the equation y = x - sqrt((0.00125x^2 - A) / 0.00125), derived from the trapezium area formula.
-     * Applies a 35.9% exit fee on the calculated shares.
-     *
-     * @dev Throws an error if the total assets are less than the specified withdrawal amount.
-     * @param assets The amount of assets to be withdrawn.
-     * @param totalSupply The total supply of shares in the system.
-     * @return shares The calculated number of shares to be burned in the withdrawal.
-     * @return fee The exit fee included in the required shares.
-     */
-    function computeWithdraw(uint256 assets, uint256 totalSupply) public pure returns (uint256 shares, uint256 fee) {
-        UD60x18 initialSupply = ud60x18(totalSupply);
-        UD60x18 withdrawnShares = initialSupply.sub(initialSupply.powu(2).sub(ud60x18(assets).div(HALF_SLOPE)).sqrt());
-
-        shares = withdrawnShares.intoUint256();
-        fee = withdrawnShares.mul(ud60x18(0.359e18)).intoUint256();
+    function computeDeposit(uint256 assets, uint256 totalSupply) public returns (uint256 shares) {
+        totalAssetsHold = totalAssetsHold + assets;
+        uint256 totalDistribution = totalSupply + assets;
+        uint256 ratio = totalDistribution / totalAssetsHold;
+        return assets * ratio;
     }
 
-    /**
-     * @notice Computes the assets to be minted for a given number of shares using _convertToAssets.
-     * @param shares The number of shares to be minted.
-     * @param totalSupply The total supply of shares in the system.
-     * @return assets The computed assets as a uint256 value.
-     */
-    function computeMint(uint256 shares, uint256 totalSupply) public pure returns (uint256 assets) {
-        assets = _convertToAssets(ud60x18(totalSupply), ud60x18(totalSupply + shares)).intoUint256();
+    function computeMint(uint256 shares, uint256 totalSupply) public returns (uint256 assets) {
+        totalAssetsHold = totalAssetsHold + shares;
+        uint256 totalDistribution = totalSupply + shares;
+        uint256 ratio =  totalAssetsHold / totalDistribution;
+        return assets * ratio;
     }
 
-    /**
-     * @notice Computes the assets, burnShares, and exit fee for a given number of shares to be redeemed.
-     * It calculates the assets to be redeemed using the _convertToAssets function and applies a 26.4% exit fee.
-     *
-     * @param shares The number of shares to be redeemed.
-     * @param totalSupply The total supply of shares in the system.
-     * @return burnShare The number of shares to be burned.
-     * @return assets The computed assets as a uint256 value.
-     * @return fee The exit fee deducted from the redeemed assets as a uint256 value.
-     */
-    function computeRedeem(uint256 shares, uint256 totalSupply)
-    public
-    pure
-    returns (uint256 burnShare, uint256 assets, uint256 fee)
-    {
-        UD60x18 _fee = ud60x18(shares).mul(ud60x18(0.264e18));
-        UD60x18 _burnShare = ud60x18(shares).sub(_fee);
-        UD60x18 finalSupply = ud60x18(totalSupply).sub(_burnShare);
+    function burnTokens(address owner, uint256 tokens) public {
+        _burn(owner, tokens);
+    }
 
-        assets = _convertToAssets(finalSupply, ud60x18(totalSupply)).intoUint256();
-        burnShare = _burnShare.intoUint256();
-        fee = _fee.intoUint256();
+    function withdraw(uint256 assets, address receiver, address owner) external returns (uint256 shares) {
+        revert();
+    }
+
+    function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets) {
+        revert();
     }
 
     /**
@@ -281,46 +200,11 @@ contract SunCoin is ERC20ABC, ISunCoin {
     function _deposit(address receiver, uint256 assets, uint256 shares) internal {
         if (assets == 0) revert CannotBeZero();
         if (shares == 0) revert CannotBeZero();
-        if (!SDAI.transferFrom(msg.sender, address(this), assets)) {
+        if (!USDT.transferFrom(msg.sender, address(this), assets)) {
             revert TransferError();
         }
         emit Deposit(msg.sender, receiver, assets, shares);
         _mint(receiver, shares);
     }
 
-    /**
-     * @dev Withdraw/redeem common workflow.
-     */
-    function _withdraw(address receiver, address owner, uint256 assets, uint256 shares, uint256 fee) internal {
-        if (fee == 0) revert CannotBeZero();
-        if (assets == 0) revert CannotBeZero();
-        if (shares == 0) revert CannotBeZero();
-        if (totalAssets() < assets) revert Undersupply();
-        if (totalSupply() < shares) revert Undersupply();
-        if (msg.sender != owner) {
-            _spendAllowance(owner, msg.sender, shares + fee);
-        }
-        _burn(owner, shares);
-
-        if (!transfer(FEE_ACCOUNT, fee)) revert TransferError();
-        if (!SDAI.transfer(receiver, assets)) revert TransferError();
-        emit Withdraw(msg.sender, receiver, owner, assets, shares);
-    }
-
-    /**
-     * @notice Computes assets as the area under a linear curve with a simplified form of the area of a trapezium,
-     * f(x) = mx + c, and Area = 1/2 * (a + b) * h
-     * where `a` and `b` can be both f(juniorSupply) or f(seniorSupply) depending if used in minting or redeeming.
-     * Calculates area as (seniorSupply^2 - juniorSupply^2) * halfSlope, where halfSlope = (slope / 2)
-     *
-     * @param juniorSupply The smaller supply in the operation (the initial supply during mint,
-     * or the final supply during a redeem operation).
-     * @param seniorSupply The larger supply in the operation (the final supply during mint,
-     * or the initial supply during a redeem operation).
-     * @return assets The computed assets as an instance of UD60x18 (a fixed-point number).
-     */
-    function _convertToAssets(UD60x18 juniorSupply, UD60x18 seniorSupply) internal pure returns (UD60x18 assets) {
-        UD60x18 sqrDiff = seniorSupply.powu(2).sub(juniorSupply.powu(2));
-        return sqrDiff.mul(HALF_SLOPE);
-    }
 }
--- src/interfaces/ISunCoin.sol
+++ src/interfaces/ISunCoin.sol
@@ -2,26 +2,25 @@
 pragma solidity ^0.8.24;
 
 import {IERC4626} from "@openzeppelin/contracts@5.0.2/interfaces/IERC4626.sol";
-import {UD60x18} from "@prb/math@4.0.2/src/UD60x18.sol";
 
-error Undersupply();
-error CannotBeZero();
-error SlippageError();
-error TransferError();
-error RequiresM3ter();
+    error Undersupply();
+    error CannotBeZero();
+    error SlippageError();
+    error TransferError();
+    error RequiresM3ter();
 
 interface ISunCoin is IERC4626 {
+
     function safeDeposit(uint256 assets, address receiver, uint256 minSharesOut) external returns (uint256 shares);
 
     function safeWithdraw(uint256 assets, address receiver, address owner, uint256 maxSharesIn)
-        external
-        returns (uint256 shares);
+    external
+    returns (uint256 shares);
 
     function safeMint(uint256 shares, address receiver, uint256 maxAssetsIn) external returns (uint256 assets);
 
     function safeRedeem(uint256 shares, address receiver, address owner, uint256 minAssetsOut)
-        external
-        returns (uint256 assets);
-
-    function currentPrice() external returns (UD60x18);
+    external
+    returns (uint256 assets);
+    function currentPrice() external returns (uint256 price);
 }
